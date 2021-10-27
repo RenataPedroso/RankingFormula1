02e6de91bd639126ed13e4a9e291bc92dfd42b75
Renata Pedroso <renatanicollyy1@gmail.com>
2021-10-27 15:15:04 -0300

    Formula 1

---
+++ RankingFormula1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace RankingFormula1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.picE = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picE)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Posição 1",
+            "Posição 2",
+            "Posição 3",
+            "Posição 4",
+            "Posição 5",
+            "Posição 6",
+            "Posição 7"});
+            this.comboBox1.Location = new System.Drawing.Point(77, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // picE
+            // 
+            this.picE.Location = new System.Drawing.Point(12, 130);
+            this.picE.Name = "picE";
+            this.picE.Size = new System.Drawing.Size(354, 308);
+            this.picE.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picE.TabIndex = 1;
+            this.picE.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.picE);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picE)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox picE;
+    }
+}
+
---
+++ RankingFormula1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RankingFormula1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Posição 1")
+            {
+                picE.Load("Piloto1.png");
+            }
+            else if (comboBox1.Text == "Posição 2")
+            {
+                picE.Load("piloto2.png");
+            }
+            else if (comboBox1.Text == "Posição 3")
+            {
+                picE.Load("piloto3.png");
+            }
+            else if (comboBox1.Text == "Posição 4")
+            {
+                picE.Load("piloto4.png");
+            }
+            else if (comboBox1.Text == "Posição 5")
+            {
+                picE.Load("piloto5.png");
+            }
+            else if (comboBox1.Text == "Posição 6")
+            {
+                picE.Load("piloto6.png");
+            }
+            else if (comboBox1.Text == "Posição 7")
+            {
+                picE.Load("piloto7.png");
+            }
+        }
+    }
+}
